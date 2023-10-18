605dd80acc00416640910c368eb09ddcf79dd599
Carlo Mayén <carlom.116.cm@gmail.com>
2023-10-17 18:40:04 -0600

    first commit

---
+++ src/main/java/neyam/furniture/backend/furniture_store_backend/FurnitureStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package neyam.furniture.backend.furniture_store_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnitureStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnitureStoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/neyam/furniture/backend/furniture_store_backend/FurnitureStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package neyam.furniture.backend.furniture_store_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
